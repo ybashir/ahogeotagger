227cecc1af1c939f34d6be907ecee98edbdaece4
Yasser Bashir <yasser.bashir@arbisoft.com>
2020-04-22 21:55:02 +0500

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ahogeotagger", # Replace with your own username
-    version="0.1.2",
+    version="0.1.3",
     author='Yasser Bashir',
     author_email='yasser.bashir@arbisoft.com',
     description="Fast text search for large list of city/country names",
